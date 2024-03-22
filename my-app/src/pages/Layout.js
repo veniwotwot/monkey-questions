c85ac6525124f0b0b5febc9dd0844da758f167ee
Vincent Wong <72583455+veniwotwot@users.noreply.github.com>
2024-03-22 13:30:59 -0500

    00: frame

---
+++ my-app/src/pages/Layout.js
@@ -0,0 +1,37 @@
+import { Outlet, Link } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+      <div style={{height: "80px", backgroundColor: "black", display: "flex", flexDirection: "vertical", justifyContent: "space-between"}}>
+        <Link to="/" style={{color:"white", textDecoration:"none", fontSize:"24px", padding: "20px", alignSelf: "center", fontWeight: "bold"}}>Example Site</Link>
+        <div style= {{display: "flex", flexDirection: "vertical", justifyContent: "space-between", alignItems: "center"}}>
+
+            <a style={{padding: "20px"}}><Link to="/blogs" style={{color:"white", textDecoration:"none"}}>Blogs</Link></a>
+            <a style={{padding: "20px"}}><Link to="/contact" style={{color:"white", textDecoration:"none"}}>Contact</Link></a>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  )
+};
+
+
+/*
+    <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blogs">Blogs</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+*/
+
+export default Layout;
